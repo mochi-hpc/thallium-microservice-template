e00c867911dff305536710a94b8a6872c6cd6a7d
Matthieu Dorier <mdorier@anl.gov>
2024-09-24 21:17:21 +0100

    fixing bedrock module

--- src/BedrockModule.cpp
+++ src/BedrockModule.cpp
@@ -1,75 +1,62 @@
 /*
- * (C) 2020 The University of Chicago
+ * (C) 2024 The University of Chicago
  *
  * See COPYRIGHT in top-level directory.
  */
 #include "alpha/Client.hpp"
 #include "alpha/Provider.hpp"
 #include "alpha/ProviderHandle.hpp"
-#include <bedrock/AbstractServiceFactory.hpp>
+
+#include <bedrock/AbstractComponent.hpp>
 
 namespace tl = thallium;
 
-class AlphaFactory : public bedrock::AbstractServiceFactory {
+class AlphaComponent : public bedrock::AbstractComponent {
+
+    std::unique_ptr<alpha::Provider> m_provider;
 
     public:
 
-    AlphaFactory() {}
+    AlphaComponent(const tl::engine& engine,
+                    uint16_t  provider_id,
+                    const std::string& config,
+                    const tl::pool& pool)
+    : m_provider{std::make_unique<alpha::Provider>(engine, provider_id, config, pool)}
+    {}
 
-    void *registerProvider(const bedrock::FactoryArgs &args) override {
-        auto provider = new alpha::Provider(args.mid, args.provider_id,
-                args.config, tl::pool(args.pool));
-        return static_cast<void *>(provider);
+    void* getHandle() override {
+        return static_cast<void*>(m_provider.get());
     }
 
-    void deregisterProvider(void *p) override {
-        auto provider = static_cast<alpha::Provider *>(p);
-        delete provider;
+    std::string getConfig() override {
+        return m_provider->getConfig();
     }
 
-    std::string getProviderConfig(void *p) override {
-        auto provider = static_cast<alpha::Provider *>(p);
-        return provider->getConfig();
-    }
+    static std::shared_ptr<bedrock::AbstractComponent>
+        Register(const bedrock::ComponentArgs& args) {
+            tl::pool pool;
+            auto it = args.dependencies.find("pool");
+            if(it != args.dependencies.end() && !it->second.empty()) {
+                pool = it->second[0]->getHandle<tl::pool>();
+            }
+            return std::make_shared<AlphaComponent>(
+                args.engine, args.provider_id, args.config, pool);
+        }
 
-    void *initClient(const bedrock::FactoryArgs& args) override {
-        return static_cast<void *>(new alpha::Client(args.mid));
-    }
-
-    void finalizeClient(void *client) override {
-        delete static_cast<alpha::Client *>(client);
-    }
-
-    std::string getClientConfig(void* c) override {
-        auto client = static_cast<alpha::Client*>(c);
-        return client->getConfig();
-    }
-
-    void *createProviderHandle(void *c, hg_addr_t address,
-            uint16_t provider_id) override {
-        auto client = static_cast<alpha::Client *>(c);
-        auto ph = new alpha::ProviderHandle(
-                client->engine(),
-                address,
-                provider_id,
-                false);
-        return static_cast<void *>(ph);
-    }
-
-    void destroyProviderHandle(void *providerHandle) override {
-        auto ph = static_cast<alpha::ProviderHandle *>(providerHandle);
-        delete ph;
-    }
-
-    const std::vector<bedrock::Dependency> &getProviderDependencies() override {
-        static const std::vector<bedrock::Dependency> no_dependency;
-        return no_dependency;
-    }
-
-    const std::vector<bedrock::Dependency> &getClientDependencies() override {
-        static const std::vector<bedrock::Dependency> no_dependency;
-        return no_dependency;
-    }
+    static std::vector<bedrock::Dependency>
+        GetDependencies(const bedrock::ComponentArgs& args) {
+            (void)args;
+            std::vector<bedrock::Dependency> dependencies{
+                bedrock::Dependency{
+                    /* name */ "pool",
+                    /* type */ "pool",
+                    /* is_required */ false,
+                    /* is_array */ false,
+                    /* is_updatable */ false
+                }
+            };
+            return dependencies;
+        }
 };
 
-BEDROCK_REGISTER_MODULE_FACTORY(alpha, AlphaFactory)
+BEDROCK_REGISTER_COMPONENT_TYPE(alpha, AlphaComponent)
